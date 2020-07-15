c917537aba7f5f4a74fab3db276b2beb426a6874
Tuffu <lucaspaes55@gmail.com>
2020-07-15 15:44:46 -0300

    Rest testado

--- src/test/java/com/example/demo/CursomcApplicationTests.java
+++ src/test/java/com/example/demo/CursomcApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CursomcApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 }
